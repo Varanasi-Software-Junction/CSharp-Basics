732bad0da32b8b8e51cfb7fe8bbdbd14971eb349
champaksworldcreate <champaksworld@gmail.com>
2023-07-16 15:24:48 +0530

    New API Controller

---
+++ vsjnet/API/Controllers/VsjController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VsjController : Controller
+    {
+        [HttpGet]
+        public string SayHello()
+        {
+            return "Hello ";
+        }
+        
+        [HttpGet("{name}")]
+        public string SayHello(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
